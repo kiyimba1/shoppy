9691889ecdddf7b6768c4f8fcca61151b6d51d06
kiyimba1 <edwineddie54@gmail.com>
2021-03-20 22:11:23 +0300

    Add Order App

--- cart/views.py
+++ cart/views.py
@@ -6,6 +6,8 @@ from .cart import Cart
 from .forms import CartAddProductForm
 
 # Create your views here.
+
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
@@ -13,8 +15,10 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+        cart.add(product=product,
+                 quantity=cd['quantity'], override_quantity=cd['override'])
     return redirect('cart:cart_detail')
+
 
 @require_POST
 def cart_remove(request, product_id):
@@ -23,6 +27,10 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'], 'override': True})
     return render(request, 'cart/detail.html', {'cart': cart})
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 def product_list(request, category_slug=None):
@@ -14,5 +15,6 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product':product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product':product, 'cart_product_form': cart_product_form})
 
